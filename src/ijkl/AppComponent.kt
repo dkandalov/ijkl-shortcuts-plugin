912f5ad90e399d15b6d52bc6a3934f5715dc7778
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-07-27 22:43:33 +0100

    extracted event redispatch code

--- src/ijkl/AppComponent.kt
+++ src/ijkl/AppComponent.kt
@@ -7,13 +7,7 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.ApplicationComponent
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.util.SystemInfo
-import com.intellij.openapi.wm.IdeFocusManager
-import java.awt.AWTEvent
-import java.awt.Component
 import java.awt.KeyboardFocusManager
-import java.awt.event.KeyEvent
-import java.awt.event.KeyEvent.*
-import javax.swing.JTree
 
 class AppComponent: ApplicationComponent {
 
@@ -36,56 +30,11 @@ class AppComponent: ApplicationComponent {
             logger = logger
         )
 
-        initEventReDispatch()
-    }
-
-    private fun initEventReDispatch() {
-        fun KeyEvent.copy(excludeModifier: Int = ALT_MASK, keyCode: Int) =
-            KeyEvent(
-                source as Component,
-                id,
-                `when`,
-                modifiers.and(excludeModifier.inv()),
-                keyCode,
-                keyCode.toChar()
-            )
-
-        fun Component?.hasParentJTree(): Boolean =
-            if (this == null) false
-            else if (this is JTree) true
-            else parent.hasParentJTree()
-
-        val keyboardFocusManager = KeyboardFocusManager.getCurrentKeyboardFocusManager()
-
-        fun focusIsInTree(): Boolean {
-            val component = keyboardFocusManager.focusOwner ?: IdeFocusManager.findInstanceByContext(null).focusOwner
-            return component.hasParentJTree()
-        }
-
-        val eventQueue = IdeEventQueue.getInstance()
-        val dispatcher = object: IdeEventQueue.EventDispatcher {
-            override fun dispatch(e: AWTEvent): Boolean {
-                if (e !is KeyEvent) return false
-                if (e.modifiers.and(ALT_MASK) == 0) return false
-
-                val newEvent =
-                    if (e.keyCode == VK_K) (e.copy(keyCode = VK_DOWN))
-                    else if (e.keyCode == VK_I) e.copy(keyCode = VK_UP)
-                    else if ((e.keyCode == VK_L || e.keyCode == VK_J) && focusIsInTree()) {
-                        if (e.keyCode == VK_L) e.copy(keyCode = VK_RIGHT)
-                        else if (e.keyCode == VK_J) e.copy(keyCode = VK_LEFT)
-                        else error("")
-                    } else null
-
-                if (newEvent != null) {
-                    eventQueue.dispatchEvent(newEvent)
-                    return true
-                } else {
-                    return false
-                }
-            }
-        }
-        eventQueue.addDispatcher(dispatcher, ApplicationManager.getApplication())
+        initEventReDispatch(
+            eventQueue = IdeEventQueue.getInstance(),
+            keyboardFocusManager = KeyboardFocusManager.getCurrentKeyboardFocusManager(),
+            application = ApplicationManager.getApplication()
+        )
     }
 
     override fun disposeComponent() {}
---
+++ src/ijkl/event-redispatch.kt
@@ -0,0 +1,72 @@
+package ijkl
+
+import com.intellij.ide.IdeEventQueue
+import com.intellij.openapi.application.Application
+import com.intellij.openapi.wm.IdeFocusManager
+import java.awt.AWTEvent
+import java.awt.Component
+import java.awt.Event.ALT_MASK
+import java.awt.KeyboardFocusManager
+import java.awt.event.KeyEvent
+import java.awt.event.KeyEvent.*
+import javax.swing.JTree
+
+fun initEventReDispatch(
+    eventQueue: IdeEventQueue,
+    keyboardFocusManager: KeyboardFocusManager,
+    application: Application
+) {
+    val dispatcher = IjklEventDispatcher(keyboardFocusManager, onSubstituteEvent = { event ->
+        eventQueue.dispatchEvent(event)
+    })
+    eventQueue.addDispatcher(dispatcher, application)
+}
+
+private class IjklEventDispatcher(
+    val keyboardFocusManager: KeyboardFocusManager,
+    val onSubstituteEvent: (AWTEvent) -> Unit
+) : IdeEventQueue.EventDispatcher {
+
+    override fun dispatch(event: AWTEvent): Boolean {
+        if (event !is KeyEvent) return false
+        if (event.modifiers.and(ALT_MASK) == 0) return false
+
+        val newEvent =
+            if ((event.keyCode == VK_I || event.keyCode == VK_J || event.keyCode == VK_K || event.keyCode == VK_L) && focusIsInTree()) {
+                if (event.keyCode == VK_I) event.copy(keyCode = VK_UP)
+                else if (event.keyCode == VK_J) event.copy(keyCode = VK_LEFT)
+                else if (event.keyCode == VK_K) (event.copy(keyCode = VK_DOWN))
+                else if (event.keyCode == VK_L) event.copy(keyCode = VK_RIGHT)
+                else error("")
+            } else {
+                null
+            }
+
+        if (newEvent != null) {
+            onSubstituteEvent(newEvent)
+            return true
+        } else {
+            return false
+        }
+    }
+
+    fun focusIsInTree(): Boolean {
+        val component = keyboardFocusManager.focusOwner ?: IdeFocusManager.findInstanceByContext(null).focusOwner
+        return component.hasParentJTree()
+    }
+}
+
+private fun KeyEvent.copy(excludeModifier: Int = ALT_MASK, keyCode: Int) =
+    KeyEvent(
+        source as Component,
+        id,
+        `when`,
+        modifiers.and(excludeModifier.inv()),
+        keyCode,
+        keyCode.toChar()
+    )
+
+private fun Component?.hasParentJTree(): Boolean =
+    if (this == null) false
+    else if (this is JTree) true
+    else parent.hasParentJTree()
