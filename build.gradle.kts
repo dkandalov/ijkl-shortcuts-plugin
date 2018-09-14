b19360106730b7f04d492e2c2e5e02ade543dee0
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-09-14 11:14:34 +0100

    updated build.gradle to gradle 4.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,37 +6,33 @@ import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 
-buildscript {
-    repositories {
-        mavenCentral()
-        maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots/") }
-        maven { setUrl("http://dl.bintray.com/jetbrains/intellij-plugin-service") }
-    }
-    dependencies {
-        classpath("org.jetbrains.intellij.plugins:gradle-intellij-plugin:0.3.0-SNAPSHOT")
-    }
-}
-apply { plugin("org.jetbrains.intellij") }
 plugins {
     java
     idea
     kotlin("jvm").version("1.1.1")
+    id("org.jetbrains.intellij").version("0.3.9")
 }
 java {
     sourceCompatibility = VERSION_1_8
     targetCompatibility = VERSION_1_8
 }
-
 repositories {
     mavenCentral()
 }
 
-java.sourceSets {
-    "main" {
+fun sourceRoots(block: SourceSetContainer.() -> Unit) = sourceSets.apply(block)
+val SourceSet.kotlin: SourceDirectorySet
+    get() = (this as HasConvention).convention.getPlugin<KotlinSourceSet>().kotlin
+var SourceDirectorySet.sourceDirs: Iterable<File>
+    get() = srcDirs
+    set(value) { setSrcDirs(value) }
+
+sourceRoots {
+    getByName("main") {
         kotlin.srcDirs("./src")
         resources.srcDirs("./resources")
     }
-    "test" {
+    getByName("test") {
         kotlin.srcDirs("./test")
     }
 }
@@ -63,4 +59,3 @@ task(name = "runIdeWithDifferentJvm", type = RunIdeTask::class, configuration = 
     setJbreVersion("jbrex8u152b1024.10")
 })
 
-val Any.kotlin: SourceDirectorySet get() = withConvention(KotlinSourceSet::class) { kotlin }
