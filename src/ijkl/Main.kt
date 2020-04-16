43bfe26359eeeeb7bfc4bab4e10ce7d0343f2a03
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-04-16 19:25:45 +0100

    don't use deprecated ApplicationComponent

--- src/ijkl/Main.kt
+++ src/ijkl/Main.kt
@@ -1,16 +1,16 @@
 package ijkl
 
+import com.intellij.ide.AppLifecycleListener
 import com.intellij.ide.IdeEventQueue
 import com.intellij.notification.NotificationDisplayType.STICKY_BALLOON
 import com.intellij.notification.NotificationsConfiguration
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.components.ApplicationComponent
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.util.SystemInfo
 import java.awt.KeyboardFocusManager
 
-class AppComponent: ApplicationComponent {
-    override fun initComponent() {
+class Main: AppLifecycleListener {
+    override fun appFrameCreated(commandLineArgs: MutableList<String>) {
         val logger = Logger.getInstance(this.javaClass.canonicalName)
         val application = ApplicationManager.getApplication()
         NotificationsConfiguration.getNotificationsConfiguration().register(groupDisplayId, STICKY_BALLOON, true)
