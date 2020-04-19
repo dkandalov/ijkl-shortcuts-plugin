99fc90f8a4a337afad3021d5d7a68d673fd2246d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-04-19 22:15:10 +0100

    cleanup

--- src/ijkl/common.kt
+++ src/ijkl/common.kt
@@ -7,16 +7,17 @@ import com.intellij.notification.NotificationType.INFORMATION
 import com.intellij.notification.Notifications
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.application.Application
-import java.io.Closeable
 import java.io.File
 import java.io.InputStream
 
 const val groupDisplayId = "IJKL Shortcuts"
 private const val notificationTitle = "IJKL Shortcuts plugin"
 
-fun resourceInputStream(fileName: String): InputStream =
-    if (File(fileName).exists()) File(fileName).inputStream()
+fun resourceInputStream(fileName: String): InputStream {
+    val file = File(fileName)
+    return if (file.exists()) file.inputStream()
     else Main::class.java.classLoader.getResource(fileName)!!.openStream()
+}
 
 fun Application.showNotification(message: String, listener: NotificationListener = URL_OPENING_LISTENER) {
     messageBus
@@ -26,27 +27,3 @@ fun Application.showNotification(message: String, listener: NotificationListener
 
 fun ActionManager.actionText(actionId: String) =
     getAction(actionId)?.templatePresentation?.text ?: actionId
-
-
-inline fun <T : Closeable?, R> T.use(block: (T) -> R): R {
-    var exception: Throwable? = null
-    try {
-        return block(this)
-    } catch (e: Throwable) {
-        exception = e
-        throw e
-    } finally {
-        this.closeFinally(exception)
-    }
-}
-
-fun Closeable?.closeFinally(cause: Throwable?) = when {
-    this == null -> {}
-    cause == null -> close()
-    else ->
-        try {
-            close()
-        } catch (closeException: Throwable) {
-            cause.addSuppressed(closeException)
-        }
-}
