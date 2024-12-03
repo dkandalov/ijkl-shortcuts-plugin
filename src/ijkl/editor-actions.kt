bb9b2511469904167238a3691287d1032547acc2
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2024-12-03 10:26:09 +0000

    tidy up

--- src/ijkl/editor-actions.kt
+++ src/ijkl/editor-actions.kt
@@ -9,12 +9,28 @@ import com.intellij.openapi.ide.CopyPasteManager
 import ijkl.MoveType.*
 
 // Actions to which work outside of code editor, e.g. in the text search input field.
-// Ideally, they should be part of IDE TODO find/raise youtrack issue
+// See https://youtrack.jetbrains.com/issue/IJPL-172879
 
-class MoveCaretLeftAction : TextComponentEditorAction(CareMoveHandler(Left))
-class MoveCaretRightAction : TextComponentEditorAction(CareMoveHandler(Right))
-class MoveCaretLeftWithSelectionAction : TextComponentEditorAction(CareMoveHandler(LeftWithSelection))
-class MoveCaretRightWithSelectionAction : TextComponentEditorAction(CareMoveHandler(RightWithSelection))
+class MoveCaretLeftAction : TextComponentEditorAction(CaretMoveHandler(Left))
+class MoveCaretRightAction : TextComponentEditorAction(CaretMoveHandler(Right))
+class MoveCaretLeftWithSelectionAction : TextComponentEditorAction(CaretMoveHandler(LeftWithSelection))
+class MoveCaretRightWithSelectionAction : TextComponentEditorAction(CaretMoveHandler(RightWithSelection))
+
+// Based on com.intellij.openapi.editor.actions.TextStartAction
+private class CaretMoveHandler(private val moveType: MoveType) : EditorActionHandler.ForEachCaret() {
+    public override fun doExecute(editor: Editor, caret: Caret, dataContext: DataContext) {
+        when (moveType) {
+            Left               -> if (caret.offset != 0) EditorActionUtil.moveCaret(caret, caret.offset - 1, false)
+            LeftWithSelection  -> if (caret.offset != 0) EditorActionUtil.moveCaret(caret, caret.offset - 1, true)
+            Right              -> EditorActionUtil.moveCaret(caret, caret.offset + 1, false)
+            RightWithSelection -> EditorActionUtil.moveCaret(caret, caret.offset + 1, true)
+        }
+    }
+}
+
+private enum class MoveType {
+    Left, Right, LeftWithSelection, RightWithSelection
+}
 
 // Based on com.intellij.openapi.editor.actions.DeleteToWordBoundaryHandler
 class DeleteAction : TextComponentEditorAction(object : EditorWriteActionHandler.ForEachCaret() {
@@ -31,18 +47,3 @@ class DeleteAction : TextComponentEditorAction(object : EditorWriteActionHandler
     }
 })
 
-// Based on com.intellij.openapi.editor.actions.TextStartAction
-private class CareMoveHandler(private val moveType: MoveType) : EditorActionHandler.ForEachCaret() {
-    public override fun doExecute(editor: Editor, caret: Caret, dataContext: DataContext) {
-        when (moveType) {
-            Left               -> if (caret.offset != 0) EditorActionUtil.moveCaret(caret, caret.offset - 1, false)
-            LeftWithSelection  -> if (caret.offset != 0) EditorActionUtil.moveCaret(caret, caret.offset - 1, true)
-            Right              -> EditorActionUtil.moveCaret(caret, caret.offset + 1, false)
-            RightWithSelection -> EditorActionUtil.moveCaret(caret, caret.offset + 1, true)
-        }
-    }
-}
-
-private enum class MoveType {
-    Left, Right, LeftWithSelection, RightWithSelection
-}
